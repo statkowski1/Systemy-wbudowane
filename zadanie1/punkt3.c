f1541569f117724f4f83c4569b665a372d1c7656
statkowski1 <61691370+statkowski1@users.noreply.github.com>
2023-04-16 16:19:43 +0200

    Update punkt3.c

--- zadanie1/punkt3.c
+++ zadanie1/punkt3.c
@@ -5,18 +5,26 @@ int main()
 {
     int liczba = 0;
     int gray = 0;
+    int gray_tab[8];
+    int j = 0;
     while(1)
     {
+        if(liczba==255)
+        {
+            liczba = 0;
+        }
         for(int i = 7; i >= 0; i--)
         {
-            printf("%d", (gray >> i) & 1);
+            gray_tab[j] = (gray >> i) & 1;
+            j += 1;
+        }
+        j = 0;
+        for(int i=0; i<8; i++)
+        {
+            printf("%d", gray_tab[i]);
         }
         printf("\n");
-        if(liczba == 255)
-        {
-            break;
-        }
-        liczba++;
+        liczba += 1;
         gray = liczba ^ (liczba >> 1);
         usleep(900000);
     }
